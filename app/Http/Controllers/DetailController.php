5baae0deb048ff467781ff22fce23afc7df8d52c
EC2 Default User <ec2-user@ip-172-31-95-247.ec2.internal>
2020-06-24 03:10:19 +0000

    #41スポット詳細を編集

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CommentRequest;
 
 //クラスのインポート
 use App\Anime;
@@ -11,43 +12,40 @@ use App\Location;
 use App\Comment;
 use App\User;
 
+use App\Services\FunctionService;
+use Illuminate\Support\Facades\DB;
+
+
 class DetailController extends Controller
 {
-    //スポット詳細
-    
+
     //投稿詳細
-    public function detail($id)
+    public function detail(FunctionService $service, $id)
     {
         $spots = Spot::where('id', $id)->get();
-        foreach($spots as $for_table) { }
-        $animes = Anime::where('id', $for_table->anime_id)->get();
-        $locations = Location::where('id', $for_table->location_id)->get();
         $comments = Comment::where('spot_id', $id)->get();
-        foreach($comments as $for_user) { }
-        $users = User::where('id', $for_user->user_id)->get();
+        $datas = DB::table('spots')
+                ->join('locations','spots.location_id','=','locations.id')
+                ->select('locations.lat','locations.lng')
+                ->where('spots.id', $id)
+                ->get();
+        $spots_json = $service->getSpotsJson($datas);
         return view('spot_detail', [
           'title' => 'スポット詳細',
           'spots' => $spots,
-          'animes' => $animes,
-          'locations' => $locations,
           'comments' => $comments,
-          'users' => $users,
+          'spots_json' => $spots_json,
         ]);
     }
     
-    // //コメント投稿
-    // public function spots(Request $request) {
-    //     // Commentモデルを利用して空のCommentオブジェクトを作成
-    //     $new_comment = new Comment;
-        
-    //     // フォームから送られた値でcommentとspot_idを設定
-    //     $new_comment->comment = $request->comment;
-    //     $new_comment->id = $request->spot_id;
-        
-    //     // テーブルにINSERT
-    //     $new_comment->save();
-
-    //     // メッセージ一覧ページにリダイレクト
-    //     return redirect('/detail');
-    // }
+    //コメントクリエイト
+    public function store(CommentRequest $request) {
+        Comment::create([
+            'comment' => $request->comment,
+            'spot_id' => $request->spot_id,
+            'user_id' => \Auth::user()->id,
+        ]);
+        \Session::flash('success', 'コメントを追加しました');
+        return redirect('/detail/' . $request->spot_id);
+    }
 }
--- resources/views/spot_detail.blade.php
+++ resources/views/spot_detail.blade.php
@@ -3,88 +3,147 @@
 @section('title', $title)
 
 @section('content')
-<!--<h1>{{ $title }}</h1>-->
+<h1>{{ $title }}</h1>
+<h2 id="detail_h2"></h2>
+@foreach($spots as $spot)
+<h2 id="detail_h2">{{ $spot->name }}</h2>
+<div id="detail_page_map_box"></div>
+<img class="detail_img" src="{{ asset('upload_image/' . $spot->spot_image) }}"></img>
+<table class="detail_table">
+    <tr>
+        <th>シーン</th>
+        <td>{{ $spot->spot_content }}</td>
+        
+    </tr>
+    <tr>
+        <th>営業施設</th>
+        <td>{{ $spot->business_name }}</td>
+    </tr>
+    <tr>
+        <th>営業時間</th>
+        <td>{{ $spot->business_time }}</td>
+    </tr>
+    <tr>
+        <th>価格</th>
+        <td>{{ $spot->price }}円</td>
+    </tr>
+    <tr>
+        <th>営業内容</th>
+        <td>{{ $spot->business_content }}</td>
+    </tr>
+    <tr>
+        <th>施設画像</th>
+        <td><img class="detail_business_img" src="{{ asset('upload_image/' . $spot->business_image) }}"></img></td>
+    </tr>
+</table>
+@endforeach
 
-<!--<?php-->
-<!--$user_name = 'aaa';-->
-<!--$user_id = 1;-->
-<!--?>-->
+<label class="comment_label">
+<button id="add_comment_btn" class="add_comment_title" data-toggle="modal" data-target="#popup">口コミを投稿</button>
+<div class="modal fade" id="popup" tabindex="-1" role="dialog" aria-labelledby="popupLabel">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="popupLabel">ユーザー名:{{ \Auth::user()->name }}さん</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="{{ url('/detail') }}" name="cnvForm">
+            @csrf
+            <div class="form-group">
+                <label class="col-form-label">口コミ内容:
+                    <textarea name="comment" rows="5" cols="30" wrap="hard" onKeyUp="checkNum()" class="form-control"></textarea>
+                </label>
+             </div>
+            <input type="hidden" name="spot_id" value="{{ $spots[0]->id }}"/>
+            <input type="submit" value="投稿する" role="button" class="btn-dark">
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+      </div>
+    </div>
+  </div>
+</div>
+</label>
 
-<!--<p><?php var_dump($users); ?> </p>-->
+@if(count($comments)>0)
+<p id="comment_caption">口コミ一覧</p>
+<div class="comment_list_scroll">
+    <table id="comment_table">
+        <tr>
+            <th>ユーザー名</th>
+            <th><div class="comment_detail">口コミ</div></th>
+            <th>投稿日時</th>
+        </tr>
+@foreach($comments as $comment)
+            <tr>
+                <td>{{ $comment->user->name }}</td>
+                <td><div class="comment_detail">{{ $comment->comment }}</div></td>
+                <td>{{ $comment->created_at }}</td>
+            </tr>
+@endforeach
+    </table>
+</div>
+@endif
+<div class="return_div"><a  class="return_link" href="{{ url('/top') }}">TOPへ戻る</a></div>
+<script src="http://libs.baidu.com/jquery/1.11.3/jquery.min.js"></script>
+<script src="{{ asset('JS/main_js.js') }}"></script>
+<script>
+    var user_name;
+    var map_spot;
+    function initMap(){
+        user_name = '{{ \Auth::user()->name }}';
+        map_spot = JSON.parse('<?php print $spots_json; ?>');
+        var map_box = document.getElementById('detail_page_map_box');
+        console.log(map_spot);
+        var mapCenter = {
+            lat: parseFloat(map_spot[0]['lat']),
+            lng: parseFloat(map_spot[0]['lng'])
+        };
+        var map = new google.maps.Map(
+              map_box,
+              {
+                center: mapCenter,
+                zoom: 16,
+                disableDefaultUI: true,
+                zoomControl: true,
+                clickableIcons: false,
+              }
+        )
+        
+        var marker = new google.maps.Marker({
+            map: map,
+            position: mapCenter,
+        });
+        
+        document.getElementById('add_comment_btn').addEventListener('click',login_judge,false)
+    }
+    function login_judge(){
+        if(user_name==''){
+            if (confirm("ログインして下さい！")){
+                window.location='/login';
+            }else{
+                window.location.reload();
+            }
+        }
+    }
 
-<p><?php foreach($spots as $spot) { } ?></p>
-<p><?php foreach($animes as $anime) { } ?></p>
-<p><?php foreach($locations as $location) { } ?></p>
-<p><?php foreach($comments as $comment) { } ?></p>
-<p><?php foreach($users as $user) { } ?></p>
+    function checkNum(){
+    	var txt = document.cnvForm.comment.value;
+    	var result = "";
+    	for (i=0; i<txt.length; i++){
+        	if (txt.match(/[<>]/)===null){
+        		result =  txt;
+        	}else{
+        	    result = "";
+            }
+        }
+    	document.cnvForm.comment.value = result;
+    }
+</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{config('api.api_key')}}&libraries=places&callback=initMap"></script>
 
-<!--<p><?php print($spot->business_name); ?></p>-->
-
-<h2 id="detail_h2"><?php print $spot['anime_name']; ?></h2>
-            <h2 id="detail_h2"><?php print $spot['id'] . '：' . $spot['spot_name']; ?></h2>
-            <div id="detail_page_map_box"></div>
-            <img class="detail_img" src="{{ asset('upload_image/' . $spot->spot_image) }}"></img>
-            <table class="detail_table">
-                <tr>
-                    <th>シーン</th>
-                    <td><?php print $spot['spot_content']; ?></td>
-                    
-                </tr>
-                <tr>
-                    <th>営業施設</th>
-                    <td><?php print $spot['business_name']; ?></td>
-                </tr>
-                <tr>
-                    <th>営業時間</th>
-                    <td><?php print $spot['business_time']; ?></td>
-                </tr>
-                <tr>
-                    <th>価格</th>
-                    <td><?php print $spot['price'] . '円'; ?></td>
-                </tr>
-                <tr>
-                    <th>営業内容</th>
-                    <td><?php print $spot['business_content']; ?></td>
-                </tr>
-                <tr>
-                    <th>施設画像</th>
-                    <td><img class="detail_business_img" src="{{ asset('upload_image/' . $spot->business_image) }}"></img></td>
-                </tr>
-            </table>
-            <label class="comment_label">
-                <span id="add_comment_btn" class="add_comment_title">口コミを投稿</span>
-                <input type="checkbox" name="checkbox" id=comment_checkbox>
-                <div id="popup">
-                    <label for="comment_checkbox" class="icon-close">×</label>
-                    <p>ユーザー名:ユーザーネームを入れるさん</p>
-                    <form method="post" action="{{ url('/detail/{id}') }}" name="cnvForm">
-                        <label class="comment_form">
-                            口コミ内容:<br>
-                            <textarea name="comment" rows="3" cols="30" wrap=”hard” onKeyUp="checkNum()" value=""></textarea>
-                        </label><br>
-                        <input type="hidden" name="spot_id" value="<?php print $spot['id'];?>"/>
-                        <input type=submit name="submit" value="投稿する">
-                    </form>
-                </div>
-            </label>
-<?php       if(count($comments)>0) { ?>
-                <p id="comment_caption">口コミ一覧</p>
-                <div class="comment_list_scroll">
-                    <table id="comment_table">
-                        <tr>
-                            <th>ユーザー名</th>
-                            <th><div class="comment_detail">口コミ</div></th>
-                            <th>投稿日時</th>
-                        </tr>
-<?php                   foreach($comments as $comment) { ?>
-                            <tr>
-                                <td><?php print htmlspecialchars($user['name'], ENT_QUOTES, 'UTF-8'); ?></td>
-                                <td><div class="comment_detail"><?php print htmlspecialchars($comment['comment'], ENT_QUOTES, 'UTF-8'); ?></div></td>
-                                <td><?php print htmlspecialchars($comment['created_at'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            </tr>
-<?php                   } ?>
-                    </table>
-                </div>
-<?php        } ?>
-                <div class="return_div"><a  class="return_link" href="main.php">TOPへ戻る</a></div>
 @endsection
