8f14c654e33277509d6b66aca719bb94d90eadeb
EC2 Default User <ec2-user@ip-172-31-80-23.ec2.internal>
2020-06-24 01:49:31 +0000

    ログイン関連リダイレクト修正、ヘッダー修正

--- resources/views/layouts/logged_in.blade.php
+++ resources/views/layouts/logged_in.blade.php
@@ -6,13 +6,13 @@
         <a href="{{ url('top') }}">
             <img class="logo" src="{{ asset('logo/logo.png') }}"></img>
         </a>
+        <div class="header_menu">ユーザー名:{{ Auth::user()->name }}さん</div>
         <div class="header_menu header_spe">
             <form name="logout" action="{{ route('logout') }}" method="POST">
                 @csrf
-                <a href="javascript:logout.submit()">ログアウト</a>
+                <a href="javascript:logout.submit()" class="header_link">ログアウト</a>
             </form>
         </div>
-        <div class="header_menu">ユーザー名:{{ Auth::user()->name }}さん</div>
         <div class="header_menu header_spe"><a class="header_link" href="">ルート探索</a></div>
         <div class="header_menu header_spe"><a class="header_link" href="{{url('/mapSpotsList')}}">聖地一覧</a></div>
     </div>
